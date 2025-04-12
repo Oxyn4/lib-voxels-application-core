2676600f21cf7df7f39e2f48ad609c0158b88660
jacob <77414989+Oxyn4@users.noreply.github.com>
2025-04-12 20:41:01 +0100

    move application types into core crate

---
+++ src/application.rs
@@ -0,0 +1,74 @@
+use std::path::PathBuf;
+
+use serde::{Serialize, Deserialize};
+use crate::filesystem::FsInt;
+
+#[derive(Debug, Copy, Clone, PartialEq, Ord, PartialOrd, Eq, Hash, Serialize, Deserialize)]
+pub enum ApplicationErrors {
+    InvalidName,
+}
+
+#[derive(Debug, Copy, Clone, PartialEq, Ord, PartialOrd, Eq, Hash, Serialize, Deserialize)]
+pub enum ApplicationsType {
+    Client,
+    Server,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, Default)]
+pub struct ApplicationRDN {
+    name: String,
+}
+
+impl ApplicationRDN {
+    pub fn new(name: String) -> Result<ApplicationRDN, ApplicationErrors> {
+        if name.len() > 255 || name.is_empty() {
+            return Err(ApplicationErrors::InvalidName);
+        }
+
+        if name.chars().filter(|&c| c == '.').count() == 0 {
+            return Err(ApplicationErrors::InvalidName);
+        }
+
+        for element in name.split('.') {
+            if element.is_empty() {
+                return Err(ApplicationErrors::InvalidName);
+            }
+
+            if element.chars().nth(0).unwrap().is_numeric() {
+                return Err(ApplicationErrors::InvalidName);
+            }
+        }
+
+        if name.chars().filter(|&c| !c.is_alphanumeric() && c != '_' && c != '.').count().gt(&0) {
+            return Err(ApplicationErrors::InvalidName);
+        }
+
+        Ok(ApplicationRDN {
+            name
+        })
+    }
+
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
+
+#[derive(Debug, Clone, Default, Serialize, Deserialize)]
+pub struct Application {
+    #[serde(skip)]
+    rdn: ApplicationRDN,
+    homepage: Option<url::Url>,
+    description: Option<String>,
+    app_type: Option<ApplicationsType>
+}
+
+impl Application {
+    pub fn new(rdn: ApplicationRDN, homepage: Option<url::Url>, description: Option<String>, app_type: Option<ApplicationsType>) -> Application {
+        Application {
+            rdn,
+            homepage,
+            description,
+            app_type
+        }
+    }
+}
